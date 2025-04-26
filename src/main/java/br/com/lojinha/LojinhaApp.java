2e5f335da496518c64d9089a9f990bcb1d35a8ee
Your Name <you@example.com>
2025-04-26 11:42:50 -0300

    Iniciando projeto lojinha

---
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -0,0 +1,16 @@
+package br.com.lojinha;
+
+public class LojinhaApp {
+    public static void main(String[] args) {
+        String joias = "100gr de Ouro 50k";
+        int minhaIdade = 34;
+        double meuSalario = 3000.99;
+        boolean minhaResposta = false;
+
+        System.out.println(joias);
+        System.out.println(minhaIdade);
+        System.out.println(meuSalario);
+        System.out.println(minhaResposta);
+
+    }
+}
